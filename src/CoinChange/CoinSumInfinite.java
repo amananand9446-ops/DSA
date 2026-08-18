b4e1692deacce22e391e2dcc66288439d200e2d2
Aman Anand <amananand9446@gmail.com>
2026-08-18 19:53:11 +0530

    Add CoinSumInfinite soln

---
+++ src/CoinChange/CoinSumInfinite.java
@@ -0,0 +1,31 @@
+package CoinChange;
+
+public class CoinSumInfinite {
+public int coinchange2(int[] A, int B) {
+    return countChange2(A, B, 0);
+}
+
+public int countChange2(int[] A, int n, int lui) {
+
+    if (n == 0) {
+        return 1;
+    }
+
+    if (n < 0) {
+        return 0;
+    }
+
+    int ways = 0;
+
+    for (int i = lui; i < A.length; i++) {
+
+        int coin = A[i];
+
+        int answer = countChange2(A, n - coin, i);
+
+        ways = ways + answer;
+    }
+
+    return ways;
+}
+}
---
+++ src/CoinChange/Main2.java
@@ -0,0 +1,14 @@
+package CoinChange;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        CoinSumInfinite solution = new CoinSumInfinite();
+
+        int[] A = {1, 2, 3};
+        int B = 4;
+
+        System.out.println(solution.coinchange2(A, B));
+    }
+}
